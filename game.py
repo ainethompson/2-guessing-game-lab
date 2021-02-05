779e9b4a01936d5abc95dcaf6574713e4d76974e
Áine Thompson <ainegthompson@gmail.com>
2021-02-05 03:32:52 +0000

    Display player greeting

--- game.py
+++ game.py
@@ -1,4 +1,21 @@
 
 """A number-guessing game."""
 
+#Pseudocode:
+
+# greet player
+# get player name
+# choose random number between 1 and 100
+# repeat forever:
+#     get guess
+#     if guess is incorrect:
+#         give hint
+#         increase number of guesses
+#     else:
+#         congratulate player
+
+user_name = input("Howdy, what's your name?\n")
+
+print(f"{user_name}, I'm thinking of a number between 1 and 100.\nTry to guess my number.")
+
 
