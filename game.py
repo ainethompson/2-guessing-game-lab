981fdceeadd858c5215860fc3612be71c7bca1fb
Áine Thompson <ainegthompson@gmail.com>
2021-02-05 04:04:44 +0000

    Account for user input errors

--- game.py
+++ game.py
@@ -24,14 +24,21 @@ num = randint(1, 100)
 num_guesses = 0
 
 while True:
-    guess = int(input("Your guess?\n"))
-    if guess > num:
-        print("Too high")
-        num_guesses += 1
-    elif guess < num:
-        print("Too low")
-        num_guesses += 1
-    elif guess == num:
-        num_guesses += 1
-        print(f"Congrats, {user_name}! You guessed my number in {num_guesses} tries.")
-        break
+    try:
+        guess = int(input("Your guess?\n"))
+
+        if guess not in range(1, 100):
+            print("So you don't know how to count? Try again!")
+        elif guess > num:
+            print("Too high")
+            num_guesses += 1
+        elif guess < num:
+            print("Too low")
+            num_guesses += 1
+        elif guess == num:
+            num_guesses += 1
+            print(f"Congrats, {user_name}! You guessed my number in {num_guesses} tries.")
+            break
+
+    except ValueError:
+        print("That's not even a number... Try again!")
